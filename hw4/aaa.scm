bcade2f5f735754e04ebacd745e63689e056703e
Ahmet Omer Kayabasi <ahmetomer@sabanciuniv.edu>
2021-05-08 16:26:03 +0300

    hw4 added

--- hw4/aaa.scm
+++ hw4/aaa.scm
@@ -1,84 +1,30 @@
-(define twoOperatorCalculator
-(lambda (slst)
-    (if (eq? (length slst) 1) 
-        (car slst)
-        (
-            let (
-                (first (car slst))
-                (sym (cadr slst))
-                (rest (cddr slst))
-            )
-            ( if (eq? sym '+) 
-                (+ first (twoOperatorCalculator rest)) 
-                (
-                    if (eq? (length rest) 1) 
-                        (- first (car rest))
-                        (+ (- first (car rest)) (twoOperatorCalculator (cddr rest) ))
-                ) 
-            )
-
-        )
-    )
-)
-)
-
-(define fourOperatorCalculator 
-(lambda (slst)
-    (
-        if (eq? (length slst) 1) 
-        slst
-        (
-                let (
-                (first (car slst))
-                (sym (cadr slst))
-                (rest (cddr slst))
-
-            )
-            ( if (eq? sym '*) 
-                (if (eq? (length rest) 1) 
-                    (list (* first (car rest)) )
-                    (fourOperatorCalculator( append ( append( list(* first (car rest)) (cadr rest)) (fourOperatorCalculator (cddr rest))) ))
-                )
-                (if (eq? sym '/) 
-                    (if (eq? (length rest) 1)
-                        (list (/ first (car rest)))
-                        (fourOperatorCalculator ( append ( append( list(/ first (car rest)) (cadr rest)) (fourOperatorCalculator (cddr rest))) ))
-                    )
-                    (append (list first sym) (fourOperatorCalculator rest))
-                
-                )
-            )
-        )
-    )
-)
-)
-
 (define calculatorNested 
-(lambda (slst) (
-    if (eq? (length slst) 1) 
-    ( if (symbol? (car slst))
-        (calculatorNested(car slst))    
-        (twoOperatorCalculator(fourOperatorCalculator (car slst)))
-    )
-    (
-        let (
-        (first (car slst))
-        (sym (cadr slst))
-        (rest (cddr slst)))
+(lambda(lst)
+    (cond
+        (   
+            (null? lst) 
+            #f
+        )
         (
-            
-            if(symbol? first)
-                (list 1)
-                (append (list first sym)  (calculatorNested rest) )
+            (and (eq? (length lst) 1) 
+                (number? (car lst))) 
+                (list (car lst))
+        )
+        (
+            (and (list? (car lst))
+                (not (null? (cdr lst))))
+                (cons (twoOperatorCalculator (fourOperatorCalculator (calculatorNested (car lst)))) (cons (cadr lst) (calculatorNested (cddr lst))))
+        )
+        (
+            (and (list? (car lst)) (null? (cdr lst))) (list (twoOperatorCalculator (fourOperatorCalculator (calculatorNested (car lst)))))
+        )
+        (
+            (and (not (list? (car lst))) (not (null? (cdr lst)))) 
+                (cons (car lst) (cons (cadr lst) (calculatorNested (cddr lst))))
+        )
+        (
+            else (list (car lst))
         )
     )
-    )
-
 )
 )
-
-
-
-(calculatorNested '( 1 + (1 + 1) )      )
-(calculatorNested '( 1 + 2 * ( 2 - ( 3 + 5 ) / 2 ) ) )  
-(calculatorNested '( 1 + 2 * ( 2  / 2 ) ) )  
--- hw4/hw4.scm
+++ hw4/hw4.scm
@@ -54,31 +54,87 @@
 )
 
 (define calculatorNested 
-    (lambda (slst) (
-        if (eq? (length slst) 1) 
-        ( if (symbol? (car slst))
-            (calculatorNested(car slst))    
-            (list twoOperatorCalculator(fourOperatorCalculator (car slst)))
-        )
-        (
-            let (
-            (first (car slst))
-            (sym (cadr slst))
-            (rest (cddr slst)))
+    (lambda(slst)
+        (cond
+            (   
+                (null? slst) 
+                #f
+            )
             (
-                
-                if(symbol? first)
-                    (list 1)
-                    (append (list first sym)   (calculatorNested rest) )
+                (and (eq? (length slst) 1) 
+                    (number? (car slst))) 
+                    (list (car slst))
+            )
+            (
+                (and (list? (car slst))
+                    (not (null? (cdr slst))))
+                    (cons   (twoOperatorCalculator (fourOperatorCalculator (calculatorNested (car slst)))) 
+                            (cons (cadr slst) (calculatorNested (cddr slst)))
+                    )
+            )
+            (
+                (and (list? (car slst)) (null? (cdr slst))) (list (twoOperatorCalculator (fourOperatorCalculator (calculatorNested (car slst)))))
+            )
+            (
+                (and (not (list? (car slst))) (not (null? (cdr slst)))) 
+                    (cons (car slst) (cons (cadr slst) (calculatorNested (cddr slst))))
+            )
+            (
+                else (list (car slst))
             )
         )
-        )
-
     )
 )
 
+(define checkOperators (
+lambda(slst)
+(cond
+    ((null? slst) #f)
+    ((and (list? (car slst)) (null? (cdr slst))) (checkOperators (car slst)))
+    ((and
+        (list? (car slst)) 
+        (not (null? (cdr slst)))) 
+            (and (checkOperators (car slst)) (checkOperators (cdr slst)))
+    )
+    ((and 
+        (number? (car slst)) 
+        (not (null? (cdr slst)))) 
+    (if (not (number? (cadr slst)))
+        (checkOperators (cdr slst))
+        #f))
+    ((and 
+        (number? (car slst)) 
+        (null? (cdr slst))) #t )
+    ((not 
+        (and (or (eq? (car slst) '+) 
+        (eq? (car slst) '-) 
+        (eq? (car slst) '*) 
+        (eq? (car slst) '/ )) 
+            (not (number? (car slst)))))
+        #f
+    )
+    ((and 
+        (or (eq? (car slst) '+) 
+        (eq? (car slst) '-) 
+        (eq? (car slst) '*) 
+        (eq? (car slst) '/ )) 
+            (not (null? (cdr slst)))
+        ) 
+            (checkOperators (cdr slst)))
+    ((and (or (eq? (car slst) '+) (eq? (car slst) '-) (eq? (car slst) '*) (eq? (car slst) '/ )) (null? (cdr slst))) #f)
+    (else #t))
+)
+)
 
-
-(calculatorNested '( 1 + (1 + 1) )      )
-(calculatorNested '( 1 + 2 * ( 2 - ( 3 + 5 ) / 2 ) ) )  
-(calculatorNested '( 1 + 2 * ( 2  / 2 ) ) )  
+(define calculator (
+    lambda(slst)
+        (
+            if (null? slst)
+            #f
+            (if (checkOperators slst)
+                (twoOperatorCalculator (fourOperatorCalculator (calculatorNested slst)))
+                #f
+            )
+        )
+)
+)
